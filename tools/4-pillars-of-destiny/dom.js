3a9820a2004570e54abd30917f3a827bc91b8311
Chen-Pang He <chen.pang.he@jdh8.org>
2017-08-02 06:40:59 +0800

    Input UI of 4 pillars

--- tools/4-pillars-of-destiny/dom.js
+++ tools/4-pillars-of-destiny/dom.js
@@ -1,17 +1,21 @@
+!function(){
+
+var form = document.forms.abacus;
+var inputs = form.elements;
+
 if (!("valueAsDate" in HTMLInputElement.prototype))
 {
-	Object.defineProperty(HTMLInputElement.prototype, "valueAsDate",
+	Object.defineProperty(inputs.date, "valueAsDate",
 	{
 		get: function() { return new Date(this.value) },
-		set: function(date) { this.value = date instanceof Date ? date.toISOString().replace(/T.*/, "") : date }
+		set: function(date) { this.value = date.toISOString().replace(/T.*/, "") }
 	})
-}
 
-!function(form){
-
-function run(inputs)
-{
-	console.log(new Date(+inputs.date.valueAsDate + +inputs.time.valueAsDate));
+	Object.defineProperty(inputs.time, "valueAsDate",
+	{
+		get: function() { return new Date("1970-01-01T" + this.value + "Z") },
+		set: function(date) { this.value = /T(.*)Z/.exec(date.toISOString())[1] }
+	});
 }
 
 form.addEventListener("submit", function(ev)
@@ -19,20 +23,20 @@ form.addEventListener("submit", function(ev)
 	ev.preventDefault();
 	ev.stopPropagation();
 
-	run(this.elements);
-})
+	console.log(inputs.date.valueAsDate, inputs.time.valueAsDate);
+});
 
-if (!form.elements.longitude.value)
+if (!inputs.longitude.value)
 {
 	var january = new Date(0);
 	var july = new Date(15638400000);
 
-	form.elements.longitude.valueAsNumber = Math.max(january.getTimezoneOffset(), july.getTimezoneOffset()) / -4;
+	longitude.valueAsNumber = Math.max(january.getTimezoneOffset(), july.getTimezoneOffset()) / -4;
 
 	navigator.geolocation.getCurrentPosition(function(position)
 	{
-		form.elements.longitude.valueAsNumber = position.coords.longitude;
+		inputs.longitude.valueAsNumber = position.coords.longitude;
 	});
 }
 
-}(document.forms.abacus);
+}();
