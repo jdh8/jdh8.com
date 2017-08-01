c0d29b129286602fd4002854b36caf07170314a3
Chen-Pang He <jdh8@ms63.hinet.net>
2017-08-02 02:52:17 +0800

    Developing 4 pillars...

--- tools/4-pillars-of-destiny/dom.js
+++ tools/4-pillars-of-destiny/dom.js
@@ -7,19 +7,32 @@ if (!("valueAsDate" in HTMLInputElement.prototype))
 	})
 }
 
-function cancel(ev)
-{
-	ev.preventDefault();
-	ev.stopPropagation();
-}
-
 !function(form){
 
-form.addEventListener("submit", cancel);
+function run(inputs)
+{
+	console.log(new Date(+inputs.date.valueAsDate + +inputs.time.valueAsDate));
+}
 
 form.addEventListener("submit", function(ev)
 {
-	console.log(this.elements.date.valueAsDate);
+	ev.preventDefault();
+	ev.stopPropagation();
+
+	run(this.elements);
 })
+
+if (!form.elements.longitude.value)
+{
+	var january = new Date(0);
+	var july = new Date(15638400000);
+
+	form.elements.longitude.valueAsNumber = Math.max(january.getTimezoneOffset(), july.getTimezoneOffset()) / -4;
+
+	navigator.geolocation.getCurrentPosition(function(position)
+	{
+		form.elements.longitude.valueAsNumber = position.coords.longitude;
+	});
+}
 
 }(document.forms.abacus);
