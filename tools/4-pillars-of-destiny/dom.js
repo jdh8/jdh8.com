fe6e5599fc6fdb46d3cea63d305cb81df76172c4
Chen-Pang He <chen.pang.he@jdh8.org>
2017-08-08 14:51:57 +0800

    Improve code with widespread ES6 features

--- tools/4-pillars-of-destiny/dom.js
+++ tools/4-pillars-of-destiny/dom.js
@@ -1,29 +1,26 @@
 "use strict";
-
-!function(){
-
-var form = document.forms.abacus;
-var inputs = form.elements;
-
-form.addEventListener("submit", function(ev)
 {
-	ev.preventDefault();
-	ev.stopPropagation();
+	const form = document.forms.abacus;
+	const inputs = form.elements;
 
-	console.log(inputs.date.valueAsDate, inputs.time.valueAsDate);
-});
-
-if (!inputs.longitude.value)
-{
-	var january = new Date(0);
-	var july = new Date(15638400000);
-
-	longitude.valueAsNumber = Math.max(january.getTimezoneOffset(), july.getTimezoneOffset()) / -4;
-
-	navigator.geolocation.getCurrentPosition(function(position)
+	form.addEventListener("submit", function(ev)
 	{
-		inputs.longitude.valueAsNumber = position.coords.longitude;
-	});
-}
+		ev.preventDefault();
+		ev.stopPropagation();
 
-}();
+		console.log(inputs.date.valueAsDate, inputs.time.valueAsDate);
+	});
+
+	if (!inputs.longitude.value)
+	{
+		const january = new Date(0);
+		const july = new Date(15638400000);
+
+		longitude.valueAsNumber = Math.max(january.getTimezoneOffset(), july.getTimezoneOffset()) / -4;
+
+		navigator.geolocation.getCurrentPosition(function(position)
+		{
+			inputs.longitude.valueAsNumber = position.coords.longitude;
+		});
+	}
+}
--- tools/prime/dom.js
+++ tools/prime/dom.js
@@ -1,21 +1,18 @@
 "use strict";
-
-!function(){
-
-var prime = Prime();
-var output = document.getElementById("result").firstChild;
-
-document.getElementById("num").addEventListener("input", function()
 {
-	output.nodeValue = function(value)
+	const prime = Prime();
+	const output = document.getElementById("result").firstChild;
+
+	document.getElementById("num").addEventListener("input", function()
 	{
-		value = +value;
+		output.nodeValue = function(value)
+		{
+			value = +value;
 
-		if (+(~~value >>> 0) != value)
-			return "請輸入 32 位元正整數。";
+			if (+(~~value >>> 0) != value)
+				return "請輸入 32 位元正整數。";
 
-		return value + [" 是合數。", " 不是質數也不是合數。", " 是質數。"][prime.test(value)];
-	}(this.valueAsNumber);
-});
-
-}();
+			return value + [" 是合數。", " 不是質數也不是合數。", " 是質數。"][prime.test(value)];
+		}(this.valueAsNumber);
+	});
+}
